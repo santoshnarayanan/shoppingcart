bf81e47531993cd4a6e5282cf193ef0f727807a5
Santosh Narayanan <narayanan@debitos.com>
2024-11-11 22:21:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold mb-4">
+          Welcome to Tailwind CSS with React and TypeScript!
+        </h1>
+        <p className="text-gray-700">
+          This is a simple example of how to use Tailwind CSS in your React
+          project.
+        </p>{" "}
+      </div>{" "}
+    </div>
+  );
+}
+
+export default App;
